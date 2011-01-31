5788e52d1ede2cced5b345114b4a7c63b0b1f63e
Tristan <Tristan@DynamicClarity.com>
2011-01-31 12:43:21 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreLocator::Application.config.secret_token = '29814a3eb6286bd283bedd4b605f2dd60ce6dccfedc64218a74e2d36c3b912b10c58ee743a728fbef59aa06c71b41d023b06c6b89934c6ba071925d7b8b75ec0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StoreLocator::Application.config.session_store :cookie_store, :key => '_store_locator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StoreLocator::Application.config.session_store :active_record_store
