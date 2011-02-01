5dc3e0e6782833a21dddc2b807d15b80885a8a08
Tristan <Tristan@DynamicClarity.com>
2011-02-01 13:31:34 +1100

    Changed XML template to return shorter address

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,10 +1,13 @@
 class Store < ActiveRecord::Base
   acts_as_mappable
   
+  def address_short
+    "#{address_1} #{address_2} #{suburb}"
+  end
+  
   def address
     "#{address_1} #{address_2}, #{suburb}, #{state}, #{postcode}, Australia"
   end
-  
   
   def geocode_address
 	  loc = Store.geocode(address)
--- app/views/stores/index.xml.builder
+++ app/views/stores/index.xml.builder
@@ -1,6 +1,6 @@
 xml.instruct! 
 xml.markers do
   @stores.each do |store|
-  	xml.marker('name' => store.name, "lat" => store.lat, "lng" => store.lng, "address" => store.address_1)
+  	xml.marker('name' => store.name, "lat" => store.lat, "lng" => store.lng, "address" => store.address_short)
   end
 end
