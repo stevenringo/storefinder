8abd2bc0ca48d4c338600081794d5349b6ccacc5
Steven Ringo <git@stevenringo.com>
2011-02-24 22:12:09 +1100

    Change blueprint to real address

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,30 +1,29 @@
 require 'machinist/active_record'
 
 Store.blueprint do
-  lmaa          {""}
-  customer_code {""}
-  name          {""}
-  address_1     {""}
-  address_2     {""}
-  suburb        {""}
-  state         {""}
-  postcode      {""}
-  lat           {0.00}
-  lng           {0.00}
+  customer_code { "0213387" }
+  name          { "BWS Somewhere" }
+  address_1     { "Shop 1" }
+  address_2     { "193 Some Street" }
+  suburb        { "Someplace" }
+  state         { "NSW" }
+  postcode      { "1111" }
+  lat           { -25.0 }
+  lng           { 135.0 }
 end
 
 Store.blueprint(:valid_address) do
-  address_1     {"197 WILSON ST"}
-  address_2     {""}
-  suburb        {"NEWTOWN"}
-  state         {"NSW"}
-  postcode      {"2042"}
+  address_1     { "197 WILSON ST" }
+  address_2     { "" }
+  suburb        { "NEWTOWN" }
+  state         { "NSW" }
+  postcode      { "2042" }
 end
 
 Store.blueprint(:invalid_address) do
-  address_1     {"SHOPS 6 & 19 CAMPUS SHOPPING VILLAGE"}
-  address_2     {"MCGREGOR ROAD"}
-  suburb        {"SMITHFIELD"}
-  state         {"QLD"}
-  postcode      {"4878"}
+  address_1     { "SHOPS 6 & 19 CAMPUS SHOPPING VILLAGE" }
+  address_2     { "MCGREGOR ROAD" }
+  suburb        { "SMITHFIELD" }
+  state         { "QLD" }
+  postcode      { "4878" }
 end
